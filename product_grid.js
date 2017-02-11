901b49059954bef87c9e1dababa550d2ed6fe14a
Morgan Bailey <morgan.colbert@myemail.indwes.edu>
2017-02-11 12:45:02 -0500

    Base files

---
+++ product_grid.js
@@ -0,0 +1,9 @@
+// Find the 4 adjacent numbers that equal the highest product in a 20x20 grid.
+
+// First, read in the CSV file and place the contents in an array.
+
+
+// Start at the first number and check horizontally and vertically. Save the highest product and its facotrs. Repeat for each number in the array.
+
+
+// Tell the HTML document which 4 numbers to highlight.
