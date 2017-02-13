12c0f1e06dd595d16720a1414da01c7194119d28
Morgan Bailey <morgan.colbert@myemail.indwes.edu>
2017-02-12 19:35:03 -0500

    Read in File
    Added JavaScript to read a file into an array.

--- product_grid.js
+++ product_grid.js
@@ -1,12 +1,24 @@
 var main = function () {
+	"use strict";
 // Find the 4 adjacent numbers that equal the highest product in a 20x20 grid.
 
-// Create an array.
-var grid = [];
-
-
 // Read in the CSV file and place the contents in an array.
+var grid;
 
+function handleFileSelect(evt) {
+    var file = evt.target.files[0];
+    Papa.parse(file, { 
+    	dynamicTyping: true,
+		complete: function(results) {
+			grid = results
+			console.log(grid);
+		}
+	});
+}
+ 
+  $(document).ready(function(){
+    $("#fileInput").change(handleFileSelect);
+  });
 
 // Start at the first number and check horizontally and vertically. Save the highest product and its factors. Repeat for each number in the array.
 
